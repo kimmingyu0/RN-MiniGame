99e39b66c6f83df11e19dba4f9664972facdf473
kimmingyu <minku4820@gmail.com>
2022-11-08 00:42:38 +0900

    난수 생성 및 사용 컴포넌트 분할

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -3,7 +3,7 @@ import { Text, View, Pressable, StyleSheet } from "react-native";
 // RN의 공식 github 계정을 살펴보면 <Button>
 // 구조가 <View><Text> 조합인걸 알 수 있다.
 
-import Colors from "../constants/Colors";
+import Colors from "../../constants/Colors";
 
 const PrimaryButton = ({ children, onPress }) => {
   return (
